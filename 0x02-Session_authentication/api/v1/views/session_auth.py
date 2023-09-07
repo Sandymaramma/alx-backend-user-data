1ca4e0035a7c111fd65cbcbe1bbcb66441442115
sandymaramma <edesandraoge@gmail.com>
2023-09-08 00:11:44 +0100

    update

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ Module of session auth views
 """
-from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
 from os import getenv
